1ff0bcaf58f02753be8ffa174a796ea590b8b695
huabrandon0 <huabrandon0@gmail.com>
2018-11-14 20:36:39 -0800

    Robin's files
    Re-added Blender folder and its contents.

---
+++ Blender/randomColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomColor : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Renderer rend = GetComponent<Renderer>();
+        Color randomColor = RandomColor();
+
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", randomColor);
+
+        rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", randomColor);
+	}
+
+    Color RandomColor()
+    {
+        return new Color(Random.value, Random.value, Random.value);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
