f8c98c5cd2ea6398179b349404495cc5cd3bcec3
huabrandon0 <huabrandon0@gmail.com>
2018-11-30 22:20:01 -0800

    added loading screen transition. moved area of play 20 units up
    "moved area of play 20 units up" -- fixes weird lighting bug

--- Assets/BH/MainMenu/MainMenu.cs
+++ Assets/BH/MainMenu/MainMenu.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using BH.DesignPatterns;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameEvent[] _invokeOnStart;
+    [SerializeField] float _delayBeforeLoadingGame;
 
     void Start()
     {
@@ -13,5 +15,33 @@ public class MainMenu : MonoBehaviour
         {
             gameEvent.Invoke();
         }
+    }
+
+    // Source: https://www.youtube.com/watch?v=rXnZE8MwK-E
+    public void LoadGame()
+    {
+        StartCoroutine(AsyncLoadGame());
+    }
+
+    IEnumerator AsyncLoadGame()
+    {
+        yield return new WaitForSeconds(_delayBeforeLoadingGame);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync("SpectatorMode");
+        async.allowSceneActivation = false;
+
+        while (!async.isDone)
+        {
+            if (async.progress >= 0.9f)
+            {
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
