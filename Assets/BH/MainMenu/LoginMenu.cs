37200ae35b9c2065ff2404bdffacb90d643b9f3f
huabrandon0 <huabrandon0@gmail.com>
2018-12-01 12:43:44 -0800

    Fleshed out the login UI more

---
+++ Assets/BH/MainMenu/LoginMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BH
+{
+    public class LoginMenu : MonoBehaviour
+    {
+        string _inputUsername = "";
+        string _inputPassword = "";
+
+        public void SetUsername(string username)
+        {
+            _inputUsername = username;
+        }
+
+        public void SetPassword(string password)
+        {
+            _inputPassword = password;
+        }
+
+        public void SignIn()
+        {
+            DataManager.Instance.GetData(_inputUsername, _inputPassword, (data, err) =>
+            {
+                Debug.Log(err);
+                Debug.Log(data);
+            });
+        }
+
+        public void Register()
+        {
+            DataManager.Instance.RegisterUser(_inputUsername, _inputPassword, (err) =>
+            {
+                Debug.Log(err);
+            });
+        }
+    }
+}
